8323657a8e1b1a86bc7a0265143a6dcdafaaa02f
tenbo07 <tenbo07@gmail.com>
2014-06-27 18:12:37 +0900

    Update rvm_install.rb

--- site-cookbooks/aws/recipes/rvm_install.rb
+++ site-cookbooks/aws/recipes/rvm_install.rb
@@ -1,17 +1,14 @@
 execute 'install_rvm' do
   not_if "su - #{node[:default][:user]} -c 'which rvm'"
-
   command "su - #{node[:default][:user]} -c 'curl -L https://get.rvm.io | bash -s stable'"
 end
 
 execute 'rvm install ruby-2.0.0' do
   not_if "su - #{node[:default][:user]} -c 'rvm list | grep 2.0.0'"
-
   command "su - #{node[:default][:user]} -c 'rvm install 2.0.0'"
 end
 
 execute 'set default ruby' do
   not_if "su - #{node[:default][:user]} -c 'echo $rvm_ruby_string | grep -v system'"
-
   command "su - #{node[:default][:user]} -c 'rvm use ruby-2.0.0 --default'"
 end
